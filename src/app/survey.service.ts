487cb549b8067fda11b5a081ff9bee6db52d3f59
Traxinger Alex <ATRAXINGER@UPS.COM>
2018-04-25 11:27:13 -0400

    chore: corrects survey api url

--- src/app/survey.service.ts
+++ src/app/survey.service.ts
@@ -20,12 +20,12 @@ export class SurveyService {
   constructor(private http: HttpClient) { }
 
   getSurvey(): Observable<Survey> {
-    return this.http.get<Survey>(`${this.hostUrl}/survey/0`)
+    return this.http.get<Survey>(`${this.hostUrl}/api/survey/1`)
       .pipe(catchError(this.handleError<Survey>('getSurvey')));
   }
 
   postSurvey(survey: SurveyResponse): Observable<SurveyResponse> {
-    return this.http.post<SurveyResponse>(`${this.hostUrl}/survey`, survey, this.httpPostOptions)
+    return this.http.post<SurveyResponse>(`${this.hostUrl}/api/survey`, survey, this.httpPostOptions)
       .pipe(catchError(this.handleError<SurveyResponse>('postSurvey')));
 
   }
